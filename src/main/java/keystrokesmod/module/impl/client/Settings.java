25b026b5a92b191df00f1bc372e85af047c89330
lquifi <55291238+lquifii@users.noreply.github.com>
2025-01-25 14:07:08 +0100

    full body rotation

--- src/main/java/keystrokesmod/module/impl/client/Settings.java
+++ src/main/java/keystrokesmod/module/impl/client/Settings.java
@@ -44,7 +44,7 @@ public class Settings extends Module {
         this.registerSetting(setChatAsInventory = new ButtonSetting("Set chat as inventory", false));
         this.registerSetting(new DescriptionSetting("Rotations"));
         this.registerSetting(rotateBody = new ButtonSetting("Rotate body", true));
-        this.registerSetting(fullBody = new ButtonSetting("Full body", false));
+        this.registerSetting(fullBody = new ButtonSetting("Full body", true));
         //this.registerSetting(movementFix = new ButtonSetting("Movement fix", false));
         this.registerSetting(randomYawFactor = new SliderSetting("Random yaw factor", 1.0, 0.0, 10.0, 1.0));
         this.registerSetting(new DescriptionSetting("Profiles"));
